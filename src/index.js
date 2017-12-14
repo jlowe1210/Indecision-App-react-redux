e2d226f0dab0da5b4fabde4eddb9044e31ecea74
Justin Lowe <justin.lowe53@yahoo.com>
2017-12-14 01:32:59 -0500

    Delete index.js

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './styles/styles.css'
-import 'bootstrap/dist/css/bootstrap.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
-
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
